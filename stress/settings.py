81b982a6cb6da1205a10a14b31ab48be2ecadcc2
sabanajasmine123 <126048560+sabanajasmine123@users.noreply.github.com>
2023-04-25 10:24:29 +0530

    Update settings.py

--- stress/settings.py
+++ stress/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'employee',
+    'manager',
 
 ]
 
@@ -122,7 +123,21 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER ='projectgjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'qntzweutomlwkijo'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
