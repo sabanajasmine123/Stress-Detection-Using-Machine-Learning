3a18bb461006788e10481cf6510e5bddace5bb86
sabanajasmine123 <126048560+sabanajasmine123@users.noreply.github.com>
2023-04-25 10:25:10 +0530

    Update urls.py

--- stress/urls.py
+++ stress/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('employee/', include('employee.urls')),
+    path('manager/', include('manager.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
